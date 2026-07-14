98306f4763ca34afdbf44ca16862a5217dec7832
Leonardo-de-Moura <leonardo.2025pessoal@gmail.com>
2026-07-13 21:13:28 -0300

    learn pointers in c

---
+++ functions.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void fat(int n);
+
+int main(void){
+int n;
+printf("digite o número");
+scanf("%d", &n);
+fat(n);
+
+
+
+
+return 0;
+}
+
+void fat(int n){
+int i;
+int f=1;
+for (i=1; i<=n; i++)
+    f*=i;
+printf("Fatorial= %d\n", f);
+
+
+}
--- o1.c
+++ o1.c
@@ -1,27 +1,8 @@
 #include <stdio.h>
+int main(void){
 
-int fatorial(int n);
-
-int main(){
-int n;
-scanf("%d", &n);
-int z=fatorial(n);
-printf("este é o fatorial %d",z);
+printf("Hello World");
 
 
 return 0;
 }
-
-int fatorial(int n){
-    int f =1;
-
-    while(n>1){
-        f*=n;
-        n--;
-}
-    return f;
-};
-
-
-
-
