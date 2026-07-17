62aad7e3d4877e05dec9c628eb8ded1d01ffb58d
Leonardo-de-Moura <leonardo.2025pessoal@gmail.com>
2026-07-16 21:41:39 -0300

    up

---
+++ VetoresToFunctions.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+float media (int n, float* v){
+int i;
+float s = 0,0f;
+for (i=0; i<n;i++)
+    s+= v[i];
+
+return s/n;
+
+
+
+
+
+
+}
+float variancia(int n, float* v, float m)
