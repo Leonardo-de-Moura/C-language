e7348a6db78bc03c04f7fe969215560202a1327c
Leonardo-de-Moura <leonardo.2025pessoal@gmail.com>
2026-07-17 20:44:59 -0300

    up

--- VetoresToFunctions.c
+++ VetoresToFunctions.c
@@ -1,17 +1,35 @@
 #include <stdio.h>
 
-float media (int n, float* v){
-int i;
-float s = 0,0f;
-for (i=0; i<n;i++)
-    s+= v[i];
+float media(int n, float *v)
+{
+    int i;
+    float s = 0.0f;
+    for (i = 0; i < n; i++)
+        s += v[i];
 
-return s/n;
-
-
-
-
-
-
+    return s / n;
 }
-float variancia(int n, float* v, float m)
+
+float variancia(int n, float *v, float m)
+{
+
+    int i;
+    float s = 0.0f;
+    for (i = 0; i < n; i++)
+        s += (v[i] - m) * (v[i] - m);
+    return s / n;
+}
+
+int main(void)
+{
+    float v[10];
+    float med, var;
+    int i;
+    for (i = 0; i < 10; i++)
+        scanf("%f", &v[i]);
+
+    med = media(10, v);
+    var = variancia(10, v, med);
+    printf("media = %f variancia = %f \n", med, var);
+    return 0;
+}
