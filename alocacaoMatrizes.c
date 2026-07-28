dbf18e92569401aa4a82359908b5d1c2b9b546cc
Leonardo-de-Moura <leonardo.2025pessoal@gmail.com>
2026-07-28 18:40:01 -0300

    up

---
+++ alocacaoMatrizes.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main ( void ){
+int i;
+float **mat;
+int n,m;
+mat = (float**) malloc(m*sizeof(float*));
+for (i=0; i<m; i++)
+	mat[i] = (float*) malloc(n*sizeof(float));
+
+
+
+
+
+
+
+return 0;
+}
