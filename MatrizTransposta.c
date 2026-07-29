8f050816e12b141225fa1bb0eb038cbca5d9c11b
Leonardo-de-Moura <leonardo.2025pessoal@gmail.com>
2026-07-29 17:40:37 -0300

    up

---
+++ MatrizTransposta.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+float *transpost_simple(int m, int n, float *mat);
+
+float **transpost_pointer(int m, int n, float **mat);
+
+int main(void)
+{
+    int n,m;
+    printf("digite as dimensoes das matrizes, primeiro linhas depois colunas");
+    scanf("%d%d",&n,&m);
+    transpost_pointer(n, m);
+    transpost_simple(n,m);
+    return 0;
+}
+
+float *transpost_simple(int m, int n, float *mat)
+{ // vetor simples
+    int i, j;
+    float *trp;
+
+    trp = (float *)malloc(n * m * sizeof(float));
+
+    for (i = 0; i < m; i++)
+        for (j = 0; j < n; j++)
+            trp[j * m + i] = mat[i*n + j];
+
+    return trp;
+}
+
+float **transpost_pointer(int m, int n, float **mat)
+{
+
+    int i, j;
+    float **trp;
+
+    trp = (float **)malloc(m * sizeof(float));
+
+    for (i = 0; i < n; i++)
+        trp[i] = (float *)malloc(m * sizeof(float));
+
+    for (i = 0; i < m; i++)
+        for (j = 0; j < n; j++)
+            trp[j][i] = mat[i][j];
+
+    return trp;
+}
